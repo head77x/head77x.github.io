8cb429a4402ddbfe97ad5b3d80e346bb83def478
Brandon Chung <cto@netmego.com>
2021-11-25 19:31:18 +0900

    modify

--- webar/script.js
+++ webar/script.js
@@ -80,24 +80,22 @@ var setModel = function (model, entity) {
 function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+    let latitude = places.location.lat;
+    let longitude = places.location.lng;
 
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+    let model = document.createElement('a-entity');
+    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
 
-        setModel(models[modelIndex], model);
+    setModel(models[modelIndex], model);
 
-        model.setAttribute('animation-mixer', '');
+    model.setAttribute('animation-mixer', '');
 
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
-
-        scene.appendChild(model);
+    document.querySelector('button[data-action="change"]').addEventListener('click', function () {
+        var entity = document.querySelector('[gps-entity-place]');
+        modelIndex++;
+        var newIndex = modelIndex % models.length;
+        setModel(models[newIndex], entity);
     });
+
+    scene.appendChild(model);
 }
