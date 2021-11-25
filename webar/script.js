3344fc35939663ed22c5208ce714b62cde21eefa
Brandon Chung <cto@netmego.com>
2021-11-25 19:32:55 +0900

    modify

--- webar/script.js
+++ webar/script.js
@@ -38,7 +38,7 @@ function staticLoadPlaces() {
 
 var models = [
     {
-        url: './assets/magnemite/scene.gltf',
+        url: './assets/1.gltf',
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
