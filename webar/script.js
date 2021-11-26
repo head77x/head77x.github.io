608970fe58bcba0f0d9018f3c020b39568f4f2df
Brandon Chung <cto@netmego.com>
2021-11-26 20:06:01 +0900

    positions

--- webar/script.js
+++ webar/script.js
@@ -25,20 +25,20 @@ var models = [
         scale: '0.5 0.5 0.5',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
-        position: '0 0 0',
+        position: '0 0 10',
     },
     {
         url: './assets/magnemite/scene.gltf',
         scale: '0.2 0.2 0.2',
         rotation: '0 180 0',
-        position: '0 0 0',
+        position: '0 0 -10',
         info: 'Articuno, Lv. 80, HP 100/100',
     },
     {
         url: './assets/magnemite/scene.gltf',
         scale: '0.08 0.08 0.08',
         rotation: '0 180 0',
-        position: '0 0 0',
+        position: '0 -10 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
     },
 ];
