3641f9734e1efad0c3d1c926160cbff8552cd523
Brandon Chung <cto@netmego.com>
2021-12-15 15:27:37 +0900

    click

--- webar/script.js
+++ webar/script.js
@@ -167,6 +167,15 @@ window.onload = () => {
         }
     });
 
+		// 게임 UX에서 처음으로 돌아가기 버튼 누를때 처리
+		document.getElementById('whathomebutton').addEventListener('click', function () {
+			document.getElementById('confirmhome').style.display = 'block';
+
+			console.log('clicked here !!!');
+		});
+	
+
+
     // 최초 로딩 완료후 UX표시
     document.querySelector('a-scene').addEventListener('loaded', function () {
         document.getElementById('firstux').style.display = 'block';
