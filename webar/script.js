27a4b3aefe6837fbc3002115a5565622ff26ca58
Brandon Chung <cto@netmego.com>
2021-12-21 16:37:42 +0900

    bow

--- webar/script.js
+++ webar/script.js
@@ -167,16 +167,16 @@ AFRAME.registerComponent('brandon-shoot', {
 				let dist = (e.changedTouches[0].clientY - this.startpoint);
 
 				if ( dist > 30 && dist <= 100 ) {
-					dist = -0.02;
+					dist = -0.03;
 					document.getElementById('bow').setAttribute("animation-mixer","clip: ready1; loop: once; duration: 0.5; clampWhenFinished: true;");					
 				} else if ( dist > 100 && dist <= 200 ) {
-					dist = -0.01;
+					dist = -0.02;
 					document.getElementById('bow').setAttribute("animation-mixer","clip: ready2; loop: once; duration: 0.5; clampWhenFinished: true;");					
 				} else if ( dist > 200 ) {
-					dist = 0;
+					dist = -0.01;
 					document.getElementById('bow').setAttribute("animation-mixer","clip: ready3; loop: once; duration: 0.5; clampWhenFinished: true;");					
 				} else {
-					dist = -0.03;
+					dist = -0.04;
 					document.getElementById('bow').setAttribute("animation-mixer","clip: ready0; loop: once; duration: 0.5; clampWhenFinished: true;");					
 				}
 
