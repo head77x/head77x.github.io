d85663642f78476372acd6aa9a1abfd36b1ebe23
Brandon Chung <cto@netmego.com>
2021-12-22 20:27:57 +0900

    shot

--- webar/script.js
+++ webar/script.js
@@ -14,38 +14,13 @@ var quizs = [
 var quizidx = 0;
 
 	function saveQR() {
-	//특정부분 스크린샷
-	html2canvas(document.getElementById("qrux"))
-	//id container 부분만 스크린샷
-	.then(function (canvas) {
-	//jpg 결과값
-	drawImg(canvas.toDataURL('image/jpeg'));
-	//이미지 저장
-	saveAs(canvas.toDataURL(), 'shinhan_qr.jpg');
-	}).catch(function (err) {
-	console.log(err);
-	});
-	}
-	
-	function drawImg(imgData) {
-		console.log(imgData);
-		//imgData의 결과값을 console 로그롤 보실 수 있습니다.
-		return new Promise(function reslove() {
-		//내가 결과 값을 그릴 canvas 부분 설정
-		var canvas = document.getElementById('canvas');
-		var ctx = canvas.getContext('2d');
-		//canvas의 뿌려진 부분 초기화
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		
-		var imageObj = new Image();
-		imageObj.onload = function () {
-		ctx.drawImage(imageObj, 10, 10);
-		//canvas img를 그리겠다.
-		};
-		imageObj.src = imgData;
-		//그릴 image데이터를 넣어준다.
-		
-		}, function reject() { });
+		html2canvas(document.body)
+		.then(
+		function (canvas) {
+		saveAs(canvas.toDataURL(), 'shinhan_qr.jpg');
+		}).catch(function (err) {
+		console.log(err);
+		});
 	}
 
 	function saveAs(uri, filename) {
@@ -60,7 +35,6 @@ var quizidx = 0;
 			window.open(uri);
 		}
 	}
-
 
 function showQR() {
 document.getElementById('congratux').style.display = 'none';
@@ -160,7 +134,7 @@ AFRAME.registerComponent('brandon-hit', {
 				this.el.addEventListener('restore', this.eventHandlerFn);				
     },
     tick: function(time) {
-        if ( this.checker.childNodes.length > 1 )
+        if ( this.mydamage > 0 && this.checker.childNodes.length > 1 )
         {
 					this.checker.childNodes.forEach((who, index, sourceArr) => {
 						if ( who.object3D != null && this.el.object3D != null ) {
